0f70b0b11f9f9ba4632a86b4236b1ca74f0b3b26
yuliusc <48122458+yuliusc@users.noreply.github.com>
2021-09-24 22:41:51 +0200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Card/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
 
 <style>
+@import "styles/general.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +25,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,55 @@
+<template>
+
+  <div class="card">
+
+    <div class="cardPhoto">
+      <img src="../../public/images/photosnap.svg">
+    </div>
+
+    <div class="cardInfo">
+
+      <div class="cardCompName">
+        <p class="cardCompName__name">Photoshot</p>
+        <ul class="cardCompName__tags">
+          <li class="cardCompName__tags cardCompName__tags--new">NEW</li>
+          <li class="cardCompName__tags cardCompName__tags--featured">FEATURED</li>
+        </ul>
+      </div>
+
+      <div class="cardPosition">
+        <h2 class="cardPosition__name">Junior Frontend Developer</h2>
+      </div>
+
+      <div class="cardDetails">
+        <p class="cardDetails__date">1 day ago</p>
+        <p class="cardDetails__dot">o</p>
+        <p class="cardDetails__time"> Full Time </p>
+        <p class="cardDetails__dot">o</p>
+        <p class="cardDetails__city"> Amsterdam </p>
+
+      </div>
+
+    </div>
+
+    <div class="cardStack">
+      <ul class="cardStack__elements">
+        <li>HTML</li>
+        <li>JS</li>
+        <li>SCSS</li>
+        <li>VUE</li>
+      </ul>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Card"
+}
+</script>
+
+<style>
+@import "../styles/card.css";
+</style>
