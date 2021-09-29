8397e3948a362bc9e54bf6dfaeecc35f60a3df6d
yuliusc <48122458+yuliusc@users.noreply.github.com>
2021-09-29 23:14:02 +0200

    Add files via upload

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -11,32 +11,29 @@
       <div class="cardCompName">
         <p class="cardCompName__name">{{compName}}</p>
         <ul class="cardCompName__tags">
-          <li class="cardCompName__tags cardCompName__tags--new">NEW</li>
-          <li class="cardCompName__tags cardCompName__tags--featured">FEATURED</li>
+          <li class="cardCompName__tags cardCompName__tags--new" v-if="newO === true">NEW</li>
+          <li class="cardCompName__tags cardCompName__tags--featured" v-if="featured === true">FEATURED</li>
         </ul>
       </div>
 
       <div class="cardPosition">
-        <h2 class="cardPosition__name">Junior Frontend Developer</h2>
+        <h2 class="cardPosition__name">{{jobName}}</h2>
       </div>
 
       <div class="cardDetails">
-        <p class="cardDetails__date">1 day ago</p>
+        <p class="cardDetails__date">{{date}}</p>
         <p class="cardDetails__dot">o</p>
-        <p class="cardDetails__time"> Full Time </p>
+        <p class="cardDetails__time"> {{time}} </p>
         <p class="cardDetails__dot">o</p>
-        <p class="cardDetails__city"> Amsterdam </p>
+        <p class="cardDetails__city"> {{place}} </p>
 
       </div>
 
     </div>
 
     <div class="cardStack">
-      <ul class="cardStack__elements">
-        <li>HTML</li>
-        <li>JS</li>
-        <li>SCSS</li>
-        <li>VUE</li>
+      <ul class="cardStack__elements" v-for="tech in stack" :key="tech.compName">
+        <button @click="$emit('displayTags',tech)"> <li>{{tech}}</li></button>
       </ul>
     </div>
 
@@ -49,7 +46,16 @@ export default {
   name: "Card",
   props:{
     compName: String,
+    jobName: String,
+    date:String,
+    time:String,
+    place: String,
+    newO: Boolean,
+    featured: Boolean,
+    stack: Array
   },
+
+  emits:['displayTags'],
 }
 </script>
 
--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
+    <div class="chosenTagsDiv">
+
+      {{this.chosenTags}}
+
+    </div>
     <div v-for="job in jobs" :key="job.compName">
-      <Card :compName="job.compName"/>
+      <Card :compName="job.compName" :jobName="job.jobName" :date="job.date"
+            :time="job.time" :place="job.place" :newO="job.newO"
+            :featured="job.featured" :stack="job.stack" :class="{displayN: !job.display}"
+            @displayTags="displayTags"/>
     </div>
   </div>
 </template>
@@ -20,11 +28,11 @@ export default {
       jobs: [
         {
           compName: 'Photoshot', jobName: 'Frontend Developer', date: '1d ago', time: 'Full time', place: 'USA only',
-          new: true, featured: true, stack: ['Frontend', 'Senior', 'Html', 'Css', 'JavaScript']
+          newO: true, featured: true, stack: ['Frontend', 'Senior', 'Html', 'Css', 'JavaScript'], display: true
         },
         {
           compName: 'Manage', jobName: 'Fullstack Developer', date: '1d ago', time: 'Part Time', place: 'Remote',
-          new: true, featured: true, stack: ['Fullstack', 'Midweight', 'Python', 'Eeact']
+          newO: true, featured: true, stack: ['Fullstack', 'Midweight', 'Python', 'Eeact'], display: true
         },
         {
           compName: 'Account',
@@ -32,20 +40,41 @@ export default {
           date: '2d ago',
           time: 'Part Time',
           place: 'USA only',
-          new: true,
+          newO: true,
           featured: false,
-          stack: ['Frontend', 'Junior', 'React', 'Sass', 'JavaScript']
+          stack: ['Frontend', 'Junior', 'React', 'Sass', 'JavaScript'], display: true
         },
         {
           compName: 'MyHome', jobName: 'Junior Frontend Developer', date: '5d ago', time: 'Contract', place: 'USA only',
-          new: false, featured: false, stack: ['Frontend', 'Junior', 'CSS', 'Javascript']
+          newO: false, featured: false, stack: ['Frontend', 'Junior', 'CSS', 'Javascript'], display: true
         },
         {
           compName: 'Loop Studios', jobName: 'Software Engineer', date: '1w ago', time: 'Full Time', place: 'Worldwide',
-          new: false, featured: true, stack: ['Fullstack', 'Javascript', 'Midweight', 'Sass', 'Ruby']
+          newO: false, featured: false, stack: ['Fullstack', 'Javascript', 'Midweight', 'Sass', 'Ruby'], display: true
         }
       ],
-      nameI: 'aha'
+      nameI: 'aha',
+      chosenTags: [],
+    }
+  }
+  ,
+  methods: {
+    displayTags(tech) {
+      this.chosenTags.push(tech);
+      this.jobs.map(el => {
+
+        if (el.stack.some(r => this.chosenTags.includes(r))) {
+          el.display = true
+        } else {
+          el.display = false
+        }
+      })
+    }
+  },
+
+  watch: {
+    chosenTags() {
+      console.log('zmiana')
     }
   }
 
@@ -53,5 +82,13 @@ export default {
 </script>
 
 <style scoped>
+
+.displayN {
+  display: none;
+}
+
+.display {
+  display: flex !important;
+}
 
 </style>
